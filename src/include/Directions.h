e67357d2bc30c23eb6c0f093b7b9eac1421a83cb
rockfire32 <rockfire.f32@gmail.com>
2023-08-06 22:21:52 +0300

    Add src/include/Directions.h, enum Direction

---
+++ src/include/Directions.h
@@ -0,0 +1,15 @@
+#ifndef SNAKE_SFML_INCLUDE_DIRECTIONS_H_
+#define SNAKE_SFML_INCLUDE_DIRECTIONS_H_
+
+namespace SnakeSfml
+{
+	enum class Direction
+	{
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT,
+	};
+}
+
+#endif // SNAKE_SFML_INCLUDE_DIRECTIONS_H_
